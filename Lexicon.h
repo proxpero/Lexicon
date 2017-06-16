6bca5550dbcf571a9e00ef8f8c877b53b5e8f583
Todd Olsen <todd.olsen@me.com>
2017-06-16 08:35:05 -0400

    Initial commit.

---
+++ Lexicon.h
@@ -0,0 +1,15 @@
+//
+//  Lexicon.h
+//  Lexicon
+//
+//  Created by Todd Olsen on 6/16/17.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Lexicon.
+FOUNDATION_EXPORT double LexiconVersionNumber;
+
+//! Project version string for Lexicon.
+FOUNDATION_EXPORT const unsigned char LexiconVersionString[];
+
